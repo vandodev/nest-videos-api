532ae6bf1a4bc7083f068ca09f672336705ada8e
vandodev <vandoaparecido@hotmail.com>
2024-06-28 08:30:27 -0300

    :tv: :movie_camera: :computer: Tratando erros com ExceptionFilter

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -17,7 +17,11 @@ export class CategoriesService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+    return this.prismaService.category.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaExceptionFilter } from './exceptions-filters/prisma.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(   
+    new PrismaExceptionFilter(),    
+  );
+
   await app.listen(3000);
 }
 bootstrap();
