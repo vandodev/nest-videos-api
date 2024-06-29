3a96e2d5fc83bee998f69043b2ed161c6d7f6030
vandodev <vandoaparecido@hotmail.com>
2024-06-29 07:26:04 -0300

    :tv: :movie_camera: :computer: Update de categoria

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -27,7 +27,10 @@ export class CategoriesService {
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+    return this.prismaService.category.update({
+      where: { id },
+      data: updateCategoryDto,
+    });
   }
 
   remove(id: number) {
