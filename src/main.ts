3ddae82ed9f8525eab1d905947bc98f47c7be564
vandodev <vandoaparecido@hotmail.com>
2024-06-29 08:36:13 -0300

    :tv: :movie_camera: :computer: Validando campos com Nest

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exceptions-filters/prisma.exception-filter';
 import { CatchAllErrorsExceptionFilter } from './exceptions-filters/catch-all-errors.exception-filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,13 @@ async function bootstrap() {
   app.useGlobalFilters(   
     new CatchAllErrorsExceptionFilter(httpAdapter),
     new PrismaExceptionFilter(),    
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422, //400
+      transform: true,
+    }),
   );
 
   await app.listen(3000);
