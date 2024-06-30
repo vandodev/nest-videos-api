e11db2c5753eea6c81c0884ef70c52adb3a1dcb3
vandodev <vandoaparecido@hotmail.com>
2024-06-30 08:24:35 -0300

    :tv: :movie_camera: :computer: Criando DTO de vídeos com validação

--- src/videos/dto/create-video.dto.ts
+++ src/videos/dto/create-video.dto.ts
@@ -1,1 +1,27 @@
-export class CreateVideoDto {}
+import { Type } from 'class-transformer';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
+export class CreateVideoDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+ 
+  @IsString()
+  @IsOptional()
+  description: string | null;
+
+  @Min(1)
+  @IsInt()
+  @IsNotEmpty()
+  @Type(() => Number)
+  category_id: number;
+}
