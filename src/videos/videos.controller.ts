28ce46bc2290465fd4f35e4a2690642a5162a217
vandodev <vandoaparecido@hotmail.com>
2024-07-01 08:42:02 -0300

    :tv: :movie_camera: :computer: Validando e carregando vídeo para upload

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, ParseFilePipe,UseInterceptors, } from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
+import { VideoFileValidator } from './video-file-validator';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('videos')
 export class VideosController {
   constructor(private readonly videosService: VideosService) {}
 
   @Post()
-  create(@Body() createVideoDto: CreateVideoDto) {
+  @UseInterceptors(FileInterceptor('file'))
+  create(@Body() createVideoDto: CreateVideoDto,
+  @UploadedFile(
+    new ParseFilePipe({
+      validators: [
+        new VideoFileValidator({
+          maxSize: 1024 * 1024 * 100,
+          mimetype: 'video/mp4',
+        }),
+      ],
+      errorHttpStatusCode: 422,
+    }),
+  )
+  file: Express.Multer.File,
+) {
+    console.log(file)
     return this.videosService.create(createVideoDto);
   }
 
